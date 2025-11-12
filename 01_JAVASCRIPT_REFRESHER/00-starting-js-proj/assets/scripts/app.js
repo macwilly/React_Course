0ab86140650e9ef4b0f6b46e0f80134ad81f4fdb
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-12 17:43:21 -0500

    Going over multiple exports and aliasing

--- 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/app.js
+++ 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/app.js
@@ -6,6 +6,21 @@ import {apiKey} from "./util.js";
 // when importing a default value, you must assign a name on the import
 import apiKeyTwo from "./default_export.js"
 import {cheese} from "./default_export.js"
+
+// With multiple named exports you can import all through a csv list
+// You can also alias as you are importing.
+// import {cheeseTwo, cheeseThree as theBigCheese} from "./multiple_named_exports.js"
+
+// Also you are able to import all exports from a file as a JavaScript object
+import * as moreCheese from "./multiple_named_exports.js";
 console.log(apiKey);
 console.log(apiKeyTwo);
 console.log(cheese);
+console.log(moreCheese);
+console.log(moreCheese.cheeseTwo);
+console.log(moreCheese.cheeseThree);
+
+/*
+console.log(cheeseTwo);
+console.log(theBigCheese);
+ */
