bc6d40e02e0b78539052d6c002b41a5d21c29b79
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-12 17:32:15 -0500

    Going over default export.

--- 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/app.js
+++ 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/app.js
@@ -3,4 +3,9 @@
 // build process will manage it.
 import {apiKey} from "./util.js";
 
+// when importing a default value, you must assign a name on the import
+import apiKeyTwo from "./default_export.js"
+import {cheese} from "./default_export.js"
 console.log(apiKey);
+console.log(apiKeyTwo);
+console.log(cheese);
---
+++ 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/default_export.js
@@ -0,0 +1,7 @@
+// With the default export you do not create a variable with "let or const"
+// also you do not assign a name. You just export the value
+// The default means this is the default value that is exported by the file.
+// you can only have one default
+// While you can have only one default export in a file you can still export other data from the file as well
+export default "tuijorwetujngfjsdklh";
+export let cheese = "cheese";
