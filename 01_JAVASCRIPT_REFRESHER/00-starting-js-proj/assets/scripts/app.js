d981eddf6bf69722699449679de6a584f76b39f7
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-12 17:24:05 -0500

    Going over the basic usage of export and import for Javascript

---
+++ 01_JAVASCRIPT_REFRESHER/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,6 @@
+// import is like use or include/require from php
+// with vanilla js you need to include the .js for the file. often with React it is left off because the
+// build process will manage it.
+import {apiKey} from "./util.js";
+
+console.log(apiKey);
