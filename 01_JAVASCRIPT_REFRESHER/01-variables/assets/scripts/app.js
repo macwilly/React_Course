5583add35eb980ac85f210cd0adb292e9d020dd1
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-13 13:17:20 -0500

    Var recap
    Variables like we know and love. Nothing different.

---
+++ 01_JAVASCRIPT_REFRESHER/01-variables/assets/scripts/app.js
@@ -0,0 +1,8 @@
+// Variables should be created with the let keyword
+let userMessage = "Hello World";
+
+console.log(userMessage)
+
+// Constants should be created with the const keyword. They are not mutable
+const secondUserMessage = "Hello Solar System";
+console.log(secondUserMessage)
