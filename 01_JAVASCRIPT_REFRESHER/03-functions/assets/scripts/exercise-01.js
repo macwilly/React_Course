9cbccbf5a8697b903ad57fc2c326d241a68ca788
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-13 20:15:48 -0500

    Functions Exercise
    In course exercise of using functions.

---
+++ 01_JAVASCRIPT_REFRESHER/03-functions/assets/scripts/exercise-01.js
@@ -0,0 +1,10 @@
+/*
+    Your Task is to write a new function that is named "combine" and have the following characteristics:
+        * Accept three input values
+        * Calculate a new value based on the three input values "a * b / c"
+        * Return the calculated result
+ */
+
+function combine(firstNumber, secondNumber, thirdNumber) {
+    return firstNumber * secondNumber / thirdNumber;
+}
