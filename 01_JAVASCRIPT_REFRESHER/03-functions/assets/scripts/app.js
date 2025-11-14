7c80c67dcbdf0016951b311adc5ab4166d2cab1d
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-13 20:30:03 -0500

    Working with Arrow functions
    Working with basic arrow functions and expporting them.

--- 01_JAVASCRIPT_REFRESHER/03-functions/assets/scripts/app.js
+++ 01_JAVASCRIPT_REFRESHER/03-functions/assets/scripts/app.js
@@ -1,8 +1,11 @@
 // basic function
-
 let userName = "MackenzieWillard";
 function greet(userName = "") {
     return "Hello World! " + userName;
 }
 console.log(greet(userName));
 
+// arrow function from export
+import greetTwo from "./arrow-functions.js";
+
+console.log(greetTwo("Mac"));
---
+++ 01_JAVASCRIPT_REFRESHER/03-functions/assets/scripts/arrow-functions.js
@@ -0,0 +1,7 @@
+// Arrow functions are used for anonymous functions. These are function that do not have a name.
+// When creating an anonymous function you will have your parameter list (name, message) then the => {<do something here>}
+// You can also export your anonymous functions as well. As the default.
+
+export default (userName) => {
+    return "Hello " + userName;
+}
