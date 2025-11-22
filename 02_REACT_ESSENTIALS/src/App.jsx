71e45efc96b36bf293c8c9c9d8cf96fae7fd4579
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-22 12:17:42 -0500

    Adding on the code from the practice

--- 02_REACT_ESSENTIALS/src/App.jsx
+++ 02_REACT_ESSENTIALS/src/App.jsx
@@ -10,6 +10,15 @@ function Header() {
         </header>
     );
 }
+// You need to ensure that the return data is in an enclosing tag like a div
+export function MainGoal() {
+    return (
+        <div>
+            <h1>Main Goal</h1>
+            <p>My Main Goal: is to lear React so that I may apply to jobs that require it!</p>
+        </div>
+    );
+}
 
 function App() {
     return (
@@ -19,6 +28,7 @@ function App() {
             <main>
                 <h2>Time to get started!</h2>
             </main>
+            <MainGoal></MainGoal>
         </div>
     );
 }
