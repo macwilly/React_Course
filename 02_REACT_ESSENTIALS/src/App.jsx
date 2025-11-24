ce2bdedc062bfd71c06ebd22d2203719ce4fdee8
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-24 17:45:54 -0500

    External data used to export and import from App
    Using the new data from the data.js file in the coreconcept area

--- 02_REACT_ESSENTIALS/src/App.jsx
+++ 02_REACT_ESSENTIALS/src/App.jsx
@@ -1,5 +1,7 @@
 import reactImage from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -49,10 +51,26 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept title="Components" description="The core UI building block." image={componentsImg} />
-                        <CoreConcept />
-                        <CoreConcept />
-                        <CoreConcept />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            image={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            image={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}
+                        />
                     </ul>
                 </section>
             </main>
